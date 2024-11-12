ef96732d6475cbbb8c7bae86afbb639b48ec72da
Jichan <ippiex0913@naver.com>
2024-11-12 18:55:28 +0900

    리스너 분리

--- server/settings.py
+++ server/settings.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv("server/.env")
 GPT_KEY = os.getenv('GPT_KEY','')
 GC_TTS_KEY = os.getenv('GC_TTS_KEY')
 FB_KEY = os.getenv('FB_KEY')
+print(FB_KEY)
--- server/start_server.py
+++ server/start_server.py
@@ -25,75 +25,32 @@ app = FastAPI()
 bucket = storage.bucket('atos-cd1.appspot.com')
 audio_blob_name = 'test_audio/'
 
-# tt.gpt_test('test text')
-
-# 텍스트 디비 변경 시 실행
-def os_text(doc_snapshot, changes, read_time):
-    global init_call_os_text
-    if init_call_os_text:
-        init_call_os_text = False  # 최초 호출 후 초기 호출 여부를 False로 변경
-        return  # 최초 호출일 경우 아무 것도 하지 않고 리턴
-    for change in changes:
-        print(f'Received text snapshot: {change.document.id} => {change.document.to_dict()}')
-        
-        if(change.type.name == 'ADDED') :
-            print("text added")
-            toTranslate = change.document.to_dict() # 바뀐 텍스트 가져오기
-
-            translated_text = tt.gpt_test(toTranslate.get('text')) # gpt api 무한루프 돌 수도 있으니까 테스트로 일단 실행
-            #translated_text = tt.gpt_translate(toTranslate.get('text'))
-
-            text_dto = {'text' : translated_text}
-            print("번역된 텍스트 : " + translated_text)
-
-            audio = tts.getTTS(translated_text)
-            blob = bucket.blob("gcTTS/"+change.document.id+".wav")
-            blob.upload_from_string(audio, content_type="audio/wav")
-            print("번역 텍스트 TTS 오디오 파일 저장")
-
-            translatedText_db.add(text_dto)
-
-            
-        ## gpt api로 번역하고 디비에 저장하기
-        ## 번역한 text tts 로 음성파일 디비에 저장하기
-
-# 음성 디비 변경 시 실행
-def os_audio(doc_snapshot, changes, read_time):
-    global init_call_os_audio
-    if init_call_os_audio:
-        init_call_os_audio = False  # 최초 호출 후 초기 호출 여부를 False로 변경
-        return  # 최초 호출일 경우 아무 것도 하지 않고 리턴
-    for change in changes:
-        print(f'Received audio snapshot: {change.document.id} => {change.document.to_dict()}')
-        
-        if(change.type.name == 'ADDED') :
-            toAnalyze = change.document.to_dict() # 바뀐 오디오 이름 가져오기
-            blob_name = audio_blob_name + toAnalyze['name'] 
-            blob = bucket.blob(blob_name)
-            audio_data = blob.download_as_bytes()
-            print("음성 파일이 바이트 변수에 성공적으로 저장되었습니다.")
-            print(f"바이트 데이터 길이: {len(audio_data)}")
-
-            with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_wav_file:
-                temp_wav_file.write(audio_data)
-                temp_wav_file_path = temp_wav_file.name # 임시 파일 경로
-                print("음성 temp파일 저장 완료")
-
-## 음성파일의 경우 storage 에 저장되므로 직접적인 변화 감지가 불가능
-## 따라서 firestore에 음성파일의 제목만을 저장하는 텍스트 디비를 만들고 이 디비의 변화를 감지
-
 # 디비
 toTranslateText_db = db.collection('toTranslateText')
 userAudio_db = db.collection('userAudio')
 translatedText_db = db.collection('translatedText')
-
-# 디비 리스너 (변화 감지)
-# text_watch = toTranslateText_db.on_snapshot(os_text)
-# audio_watch = userAudio_db.on_snapshot(os_audio)
+userData_db = db.collection('userData')
 
 @app.get('/')
 def read_root():
     return 'Server for ATOS project'
+
+@app.post('/set-user')
+async def set_user(request: Request):
+    try:
+        body = await request.json()
+
+        user_save_dto = {
+            'user_id' : body.get('user_id"'),
+            'region' : body.get('region'),
+            'sex' : body.get('sex')
+        }
+
+        user_ref = userData_db.document()
+        user_ref.set(user_save_dto)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"서버 오류: {str(e)}")
 
 
 @app.post('/translate-text') 
@@ -180,14 +137,5 @@ async def get_user_practice(user_id : str) :
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"서버 오류: {str(e)}")
-
-
-
-
-
-
-
-
-
 
 
