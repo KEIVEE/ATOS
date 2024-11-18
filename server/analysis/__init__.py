f6a66be7e9e8765f466c26d2015d4649893fa18a
Jichan <ippiex0913@naver.com>
2024-11-18 21:30:54 +0900

    analysis init 수정

--- server/analysis/__init__.py
+++ server/analysis/__init__.py
@@ -3,5 +3,5 @@ from .voice_filtering import *
 from .compare_amp import *
 from .compare_pitch import *
 from .compare_gradient import *
-from tts_analysis import *
-from user_timestamp_cal import *
+from .tts_analysis import *
+from .user_timestamp_cal import *
