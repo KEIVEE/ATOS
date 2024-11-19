0364b758cc9d380d87896f2ce2f4a3999e396be1
Jichan <ippiex0913@naver.com>
2024-11-19 16:22:50 +0900

    음성 분석 출력 print

--- server/start_server.py
+++ server/start_server.py
@@ -196,57 +196,88 @@ async def voice_analysis(user_voice: UploadFile = File(...), tts_voice: UploadFi
         tts_sampling_rate, tts_data, pitch_values_tts, time_steps_tts = extract_pitch_from_tts(tts_voice_path)
 
         word_intervals = cal_timestamp(extract_word_timestamps(user_voice_path))
-        word_intervals = ts_cal(word_intervals, text)
-
         tts_word_intervals = extract_word_timestamps(tts_voice_path)
+        print("=============타임스탬프 추출 완료=============")
+
+        word_intervals = ts_cal(word_intervals, text)
         tts_word_intervals = ts_cal(tts_word_intervals, text)
+        print("=============타임스탬프 보정 완료=============")
 
         threshold_value = 5600
         user_exceeding_words, tts_exceeding_words, max_word = compare_amplitude_differences(word_intervals, tts_word_intervals, filtered_data, tts_data, tts_sampling_rate, sampling_rate, threshold_value)
 
-        for word_info in user_exceeding_words:
-            print(f"단어: {word_info['word']}이(가) 표준어 대비 세기 차이가 강해요")
+        print("=================================================================================")
+        print("user_exceeding_words : ")
+        print(user_exceeding_words)
+        print("=================================================================================")
+        print("tts_exceeding_words : ")
+        print(tts_exceeding_words)
+        print("=================================================================================")
+        print("max_word : ")
+        print(max_word)
+        print("=================================================================================\n\n\n")
 
-        for word_info in tts_exceeding_words:
-            print(f"단어: {word_info['word']}이(가) 표준어 대비 세기 차이가 약해요")
+        # for word_info in user_exceeding_words:
+        #     print(f"단어: {word_info['word']}이(가) 표준어 대비 세기 차이가 강해요")
 
-        print(f"단어: {max_word['word']}이(가) 너무 세게 말해요.")
+        # for word_info in tts_exceeding_words:
+        #     print(f"단어: {word_info['word']}이(가) 표준어 대비 세기 차이가 약해요")
+
+        # print(f"단어: {max_word['word']}이(가) 너무 세게 말해요.")
 
         u_results,t_results = calculate_pitch_differences(
         word_intervals, tts_word_intervals, pitch_values, time_steps, pitch_values_tts, time_steps_tts)
 
-        for word_info in u_results:
-            print(f"단어: {word_info['word']}이(가) 표준어 대비 억양 차이가 심해요")
-        for word_info in t_results:
-            print(f"단어: {word_info['word']}이(가) 표준어 대비 억양 차이가 약해요")
+        print("=================================================================================")
+        print("u_results : ")
+        print(u_results)
+        print("=================================================================================")
+        print("t_results : ")
+        print(t_results)
+        print("=================================================================================\n\n\n")
+        # for word_info in u_results:
+        #     print(f"단어: {word_info['word']}이(가) 표준어 대비 억양 차이가 심해요")
+        # for word_info in t_results:
+        #     print(f"단어: {word_info['word']}이(가) 표준어 대비 억양 차이가 약해요")
 
         # 세그먼트 비교
         highest_segment, lowest_segment = compare_segments(
             word_intervals, tts_word_intervals, pitch_values, time_steps, pitch_values_tts, time_steps_tts
         )
 
-        # 결과 출력
-        if highest_segment:
-            user_segment_high, tts_segment_high, avg_user_gradient_high, avg_tts_gradient_high = highest_segment
-            print(f"음성이 TTS 대비 가장 높은 기울기를 가진 세그먼트:")
-            print(f"사용자 세그먼트: {user_segment_high}, TTS 세그먼트: {tts_segment_high}")
-            print(f"사용자 평균 기울기: {avg_user_gradient_high:.4f}, TTS 평균 기울기: {avg_tts_gradient_high:.4f}")
-            print("음성이 TTS 대비 기울기가 높아요.")
+        print("=================================================================================")
+        print("highest_segment : ")
+        print(highest_segment)
+        print("=================================================================================")
+        print("lowest_segment : ")
+        print(lowest_segment)
 
-        if lowest_segment:
-            user_segment_low, tts_segment_low, avg_user_gradient_low, avg_tts_gradient_low = lowest_segment
-            print(f"음성이 TTS 대비 가장 낮은 기울기를 가진 세그먼트:")
-            print(f"사용자 세그먼트: {user_segment_low}, TTS 세그먼트: {tts_segment_low}")
-            print(f"사용자 평균 기울기: {avg_user_gradient_low:.4f}, TTS 평균 기울기: {avg_tts_gradient_low:.4f}")
-            print("음성이 TTS 대비 기울기가 낮아요.")
-        else:
-            print("세그먼트 비교에서 차이를 찾을 수 없습니다.")
+        # 결과 출력
+        # if highest_segment:
+        #     user_segment_high, tts_segment_high, avg_user_gradient_high, avg_tts_gradient_high = highest_segment
+        #     print(f"음성이 TTS 대비 가장 높은 기울기를 가진 세그먼트:")
+        #     print(f"사용자 세그먼트: {user_segment_high}, TTS 세그먼트: {tts_segment_high}")
+        #     print(f"사용자 평균 기울기: {avg_user_gradient_high:.4f}, TTS 평균 기울기: {avg_tts_gradient_high:.4f}")
+        #     print("음성이 TTS 대비 기울기가 높아요.")
+
+        # if lowest_segment:
+        #     user_segment_low, tts_segment_low, avg_user_gradient_low, avg_tts_gradient_low = lowest_segment
+        #     print(f"음성이 TTS 대비 가장 낮은 기울기를 가진 세그먼트:")
+        #     print(f"사용자 세그먼트: {user_segment_low}, TTS 세그먼트: {tts_segment_low}")
+        #     print(f"사용자 평균 기울기: {avg_user_gradient_low:.4f}, TTS 평균 기울기: {avg_tts_gradient_low:.4f}")
+        #     print("음성이 TTS 대비 기울기가 낮아요.")
+        # else:
+        #     print("세그먼트 비교에서 차이를 찾을 수 없습니다.")
 
         # 파일 삭제
         os.remove(user_voice_path)
         os.remove(tts_voice_path)
 
         result = '성공'
+
+        # result 구성 요소
+        # 1. 보정된 timestamp (json 형식)
+        # 2. 사용자 음성과 TTS 음성의 세기 차이가 큰 단어 (json 형식)
         return result
 
     except Exception as e:
