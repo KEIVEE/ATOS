68c28dda922557636e73be2c1f56dde1aef83672
KEIVEE <kewanee01@gmail.com>
2024-11-27 20:20:17 +0900

    멘토링

---
+++ application/atos/lib/account/voice_recorder.dart
@@ -0,0 +1,26 @@
+// abstract class VoiceRecorder {
+//   abstract String path;
+//   Future<void> startRecording();
+//   abstract bool isRecording;
+// }
+
+// class HighVoiceRecorder extends VoiceRecorder {
+//   @override
+//   String path = 'high.wav';
+
+//   @override
+//   Future<void> startRecording() async {
+//     // 녹음 시작
+//   }
+// }
+
+// class LowVoiceRecorder extends VoiceRecorder {
+//   @override
+//   String path = 'low.wav';
+
+//   @override
+//   Future<void> startRecording() async {
+    
+//     // 녹음 시작
+//   }
+// }
---
+++ application/atos/lib/enums.dart
@@ -0,0 +1,5 @@
+enum TodoState {
+  translation,
+  tts,
+  translate,
+}
